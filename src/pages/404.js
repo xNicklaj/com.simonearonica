280471ab41ceaf29804bac526989d53348a1859b
Simone Aronica <simonearonica@gmail.com>
2019-09-27 19:14:17 +0200

    styles missing

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,6 +2,7 @@ import React from "react"
 import {Helmet} from 'react-helmet'
 
 import Layout from "../components/layout/layout"
+import styles from './404.module.css'
 
 const NotFoundPage = () => (
   <Layout>
