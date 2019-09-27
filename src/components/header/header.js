e8abb9845251cbfee6434ea7e6281fa5cb16a1ea
Simone Aronica <simonearonica@gmail.com>
2019-09-27 19:05:03 +0200

    Commit for master

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,10 +2,10 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-import './header.css'
+import style from './header.module.css'
 
 const Header = ({children}) => (
-  <header>
+  <header >
     {children}
   </header>
 )
