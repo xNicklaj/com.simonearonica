a1b98d758275f8584dfac068e96a21ddb845d191
Simone Aronica <simonearonica@gmail.com>
2019-09-27 16:50:31 +0200

    Merged development into master to test branch dep

---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+import './header.css'
+
+const Header = ({children}) => (
+  <header>
+    {children}
+  </header>
+)
+
+
+export default Header
