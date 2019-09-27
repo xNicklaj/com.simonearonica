bfc518662ac1044ef57e56c9e33796d0145c965e
Simone Aronica <simonearonica@gmail.com>
2019-09-27 19:11:49 +0200

    Fixed a build error

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,17 @@
 import React from "react"
+import {Helmet} from 'react-helmet'
 
 import Layout from "../components/layout/layout"
-import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Helmet>
+      <title>Page not found | Simone Aronica</title>
+    </Helmet>
+    <div className={styles.content}>
+      <h1>NOT FOUND</h1>
+      <p>Are you sure this page exists? Unfortunately, last time I did it didn't...</p>
+    </div>
   </Layout>
 )
 
