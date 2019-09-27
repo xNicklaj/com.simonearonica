d569be9b19c0b72aa1e4536c8bcf4a3a0178d7fe
Simone Aronica <simonearonica@gmail.com>
2019-09-27 19:32:25 +0200

    Added content to Heading

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -2,12 +2,6 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "../header/header"
-import GitLabOriginal from '../../icons/gitlab-original'
-import Sun from '../../icons/sun'
-import GitHubOriginal from '../../icons/github-original'
-import TwitterOriginal from '../../icons/twitter-original'
-
 import styles from "./layout.module.css"
 
 const Layout = ({ children }) => {
@@ -22,16 +16,6 @@ const Layout = ({ children }) => {
   `)
   return (
     <>
-      <div className={styles.yellowBackgroundLayer}>
-        <Header>
-          <Sun className={styles.icon}/>
-          <div className={styles.actionButtonsDiv}>
-            <GitHubOriginal className={styles.icon} />
-            <GitLabOriginal className={styles.icon}/>
-            <TwitterOriginal className={styles.icon}/>
-          </div>
-        </Header>
-      </div>
       {children}
     </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,26 @@ import React from "react"
 import {Helmet} from 'react-helmet'
 
 import Layout from "../components/layout/layout"
+import Heading from '../components/heading/heading'
 
 import './global.css'
 import styles from './index.module.css'
+import avatar from '../images/avatar.png'
 
 const IndexPage = () => (
   <Layout>
     <Helmet>
       <title>Home | Simone Aronica</title>
     </Helmet>
+    <Heading>
+      <div className={styles.avatarDiv}>
+          <img src={avatar} />
+      </div>
+      <div className={styles.textDiv}>
+          <h2>Hello, I'm Simone</h2>
+          <h2>I like to develop stuff.</h2>
+      </div>
+    </Heading>
     <div className={styles.wip}>
       <h1>The more you refresh, the more this website is being developed.</h1>
     </div>
