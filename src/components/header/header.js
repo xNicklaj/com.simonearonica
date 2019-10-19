d748c0bf1659b3748b3fb75e4d7d25b9d4287298
Simone <simonearonica@gmail.com>
2019-10-19 14:54:47 +0200

    Added dark mode based on preferred color scheme

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,7 +2,7 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-import style from './header.module.css'
+import style from './header.module.scss'
 
 const Header = ({children}) => (
   <header >
