0807aa521a61ab36fff158714acc0eab92bb412b
Simone <simonearonica@gmail.com>
2019-10-21 12:33:24 +0200

    Added onclicks to icons.

--- src/icons/html5-original.js
+++ src/icons/html5-original.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Html5Icon = (props) => (
     
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="96" height="96">
+<svg onClick={props.onClick} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="96" height="96">
     <path fill="#E65100" d="M41,5H7l3,34l14,4l14-4L41,5L41,5z"/>
     <path fill="#FF6D00" d="M24 8L24 39.9 35.2 36.7 37.7 8z"/>
     <path fill="#FFF" d="M24,25v-4h8.6l-0.7,11.5L24,35.1v-4.2l4.1-1.4l0.3-4.5H24z M32.9,17l0.3-4H24v4H32.9z"/>
