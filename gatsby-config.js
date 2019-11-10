44619a57619e63f85e513a4c12df2084886ce779
Simone Aronica <simonearonica@gmail.com>
2019-11-10 18:07:15 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,13 +32,6 @@ module.exports = {
       },
     },
     'gatsby-plugin-sass',
-    {
-      resolve: 'gatsby-plugin-robots-txt',
-      options: {
-        host: 'https://www.simonearonica.com',
-        policy: [{ userAgent: '*', allow: '/' }]
-      }
-    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
